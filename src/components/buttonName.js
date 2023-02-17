e8f5c79ee138b23e81ca13dbdb2395e2f56a608f
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-18 02:24:12 +0500

    Fix errors

--- src/components/buttonName.js
+++ src/components/buttonName.js
@@ -4,17 +4,21 @@ function Button(props) {
   Button.defaultProps = {
     name: '',
     className: '',
+    id: '',
     onClick: () => {},
   };
   Button.propTypes = {
     name: PropTypes.string,
     className: PropTypes.string,
+    id: PropTypes.string,
     onClick: PropTypes.func,
   };
-  const { name, className, onClick } = props;
+  const {
+    name, className, onClick, id,
+  } = props;
 
   return (
-    <button type="button" name={name} className={className} onClick={onClick}>
+    <button type="button" name={name} className={className} id={id} onClick={onClick}>
       {name}
     </button>
   );
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -23,44 +23,45 @@ const Calculator = () => {
   return (
     <div id="container">
       <table>
-        <tr className="display-values">
-          <th>{result}</th>
-        </tr>
-        <tr className="row">
-          <td>
-            <Button onClick={(e) => { handleClick(e); }} name="AC" className="grey" />
-            {' '}
-          </td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="+/-" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="%" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="÷" className="orange" /></td>
-        </tr>
-        <tr className="row">
-          <td><Button onClick={(e) => { handleClick(e); }} name="7" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="8" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="9" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="x" className="orange" /></td>
-        </tr>
-        <tr className="row">
-          <td><Button onClick={(e) => { handleClick(e); }} name="4" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="5" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="6" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="-" className="orange" /></td>
-        </tr>
-        <tr className="row">
-          <td><Button onClick={(e) => { handleClick(e); }} name="1" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="2" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="3" className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="+" className="orange" /></td>
-        </tr>
-        <tr className="row">
-          <td colSpan={2}>
-            {' '}
-            <Button onClick={(e) => { handleClick(e); }} name="0" className="grey" />
-          </td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="." className="grey" /></td>
-          <td><Button onClick={(e) => { handleClick(e); }} name="=" className="orange" /></td>
-        </tr>
+        <tbody>
+          <tr className="display-values">
+            <th>{result}</th>
+          </tr>
+          <tr className="row">
+            <td>
+              <Button onClick={(e) => { handleClick(e); }} name="AC" className="grey" />
+              {' '}
+            </td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="+/-" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="%" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="÷" className="orange" /></td>
+          </tr>
+          <tr className="row">
+            <td><Button onClick={(e) => { handleClick(e); }} name="7" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="8" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="9" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="x" className="orange" /></td>
+          </tr>
+          <tr className="row">
+            <td><Button onClick={(e) => { handleClick(e); }} name="4" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="5" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="6" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="-" className="orange" /></td>
+          </tr>
+          <tr className="row">
+            <td><Button onClick={(e) => { handleClick(e); }} name="1" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="2" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="3" className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="+" className="orange" /></td>
+          </tr>
+          <tr className="row">
+            <td colSpan={2}>
+              <Button onClick={(e) => { handleClick(e); }} name="0" className="grey" id="last" />
+            </td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="." className="grey" /></td>
+            <td><Button onClick={(e) => { handleClick(e); }} name="=" className="orange" /></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
