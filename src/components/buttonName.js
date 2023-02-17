575e15f075f17c52503da9102b31302135231927
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-18 02:14:58 +0500

    Add functionality

--- src/components/buttonName.js
+++ src/components/buttonName.js
@@ -1,22 +1,23 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-function Button(name, className, onClick){
-    Button.defaultProps = {
-        name: '',
-        className: '',
-        onclick : () => {}
-    }
-    Button.propTypes = {
-        name: PropTypes.string,
-        className: PropTypes.string,
-        onClick: PropTypes.func,
-    }
+function Button(props) {
+  Button.defaultProps = {
+    name: '',
+    className: '',
+    onClick: () => {},
+  };
+  Button.propTypes = {
+    name: PropTypes.string,
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+  };
+  const { name, className, onClick } = props;
 
-    return (
-        <button type="button" name={name} className={className} onClick={onClick}>
-            {name}
-        </button>
-    )
+  return (
+    <button type="button" name={name} className={className} onClick={onClick}>
+      {name}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
