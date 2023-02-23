3be33d1e7006ac573af16ff187f1d6df513dc7a2
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-24 00:27:26 +0500

    Test for Calculator component

---
+++ src/components/calculator.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Calculator from './calculator';
+
+describe('Checking page deployment', () => {
+  test('Calculator component matches snapshot', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('renders Calculator component', () => {
+    render(<Calculator />);
+    
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(screen.getByText(/AC/)).toBeInTheDocument();
+  });
+});
